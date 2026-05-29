34b9fa8162abd2d578aa7e7d478dc392e3bf089c
Muhammad Umer <muhammad.umer@buggcy.com>
2026-05-29 12:42:07 +0500

    update pool.ts

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -1,13 +1,27 @@
 import { Pool } from "pg";
-import { env } from "../config/env";
+import dotenv from "dotenv";
 
-export const db = new Pool({
-  connectionString: env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+dotenv.config();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
 });
 
-db.connect()
-  .then(() => console.log("Database connected"))
-  .catch((err) => console.log(err.message));
+const connectDB = async (): Promise<void> => {
+  try {
+    await pool.query("SELECT NOW()");
+
+    console.log("PostgreSQL Connected");
+  } catch (error) {
+    console.error("Database Connection Error");
+    console.error(error);
+
+    process.exit(1);
+  }
+};
+
+export { pool, connectDB };
